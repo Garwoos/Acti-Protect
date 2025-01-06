b428d493be81f2e12fa51da06b2e788c6dacb890
Garwoos <saminochko@gmail.com>
2025-01-06 14:46:17 +0100

    Add initial HTML structure for homepage, login, registration, and plan creation pages

---
+++ acceuil.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'accueil</title>
+    <style>
+        body {
+            color: black;
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding-top: 50px;
+        }
+        .button {
+            display: inline-block;
+            margin: 10px;
+            padding: 15px 25px;
+            font-size: 16px;
+            color: black;
+            background-color: gray;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            text-decoration: none;
+        }
+        .button:hover {
+            background-color: lightgray;
+        }
+    </style>
+</head>
+<body>
+    <h1>Bienvenue sur Acti-Protect</h1>
+    <a href="#" class="button">Créer un plan</a>
+    <a href="#" class="button">Mes plans</a>
+    <a href="#" class="button">Mes devis</a>
+    <a href="#" class="button">Aide</a>
+    <a href="#" class="button">Parrainer</a>
+</body>
+</html>
