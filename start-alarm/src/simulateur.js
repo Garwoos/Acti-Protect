07d03b4730c77c5a951825d2c399ae7af7a6bc18
Lopidicey <vpelouse@gmail.com>
2025-01-13 14:52:41 +0100

    Refactor styles and structure for login, registration, and simulation components; add NavBar component for consistent navigation

--- start-alarm/src/simulateur.js
+++ start-alarm/src/simulateur.js
@@ -1,45 +1,27 @@
 import React, { useState } from 'react';
 import GridCanvas from './components/GridCanvas';
 import Toolbar from './components/Toolbar';
+import './simulateur.css';
+import NavBar from './navBar';
 
 const Simulateur = () => {
   const [tool] = useState('wall'); // Outil sélectionné (exemple)
   const [toolInHand, setToolInHand] = useState(null); // Élément en main
 
   return (
-    <div style={styles.container}>
-      <div style={styles.gridContainer}>
-        <h1 style={styles.title}>Simulateur d'Alarme</h1>
-        <GridCanvas tool={tool} toolInHand={toolInHand} setToolInHand={setToolInHand} />
+    <div className="main-container">
+      <NavBar />
+      <div className='simulateur-container'>
+        <h1 className='simulateur-title'>Configurateur</h1>
+        <div className='simulateur-grid-container'>
+          <GridCanvas tool={tool} toolInHand={toolInHand} setToolInHand={setToolInHand} />
+        </div>
+        <div className='simulateur-toolbar'>
+          <Toolbar setToolInHand={setToolInHand} />
+        </div>
       </div>
-      <Toolbar setToolInHand={setToolInHand} />
     </div>
   );
-};
-
-const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'row',
-    height: '100vh',
-  },
-  gridContainer: {
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'relative',
-  },
-  title: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    width: '100%',
-    textAlign: 'center',
-    backgroundColor: '#fff',
-    zIndex: 1,
-  },
 };
 
 export default Simulateur;
