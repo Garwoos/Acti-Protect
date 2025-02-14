a4111c9c019c0c129f46f6a7375d60745e7a9bf7
Garwoos <saminochko@gmail.com>
2025-02-14 13:13:59 +0100

    Remove unused projects route from API routes; streamline route management

--- api-start-alarm/apiRoutes.js
+++ api-start-alarm/apiRoutes.js
@@ -4,13 +4,11 @@ const router = express.Router();
 const equipementsRoutes = require('./routes/equipements');
 const authRoutes = require('./routes/auth');
 const ouverturesRoutes = require('./routes/ouvertures');
-const projetsRoutes = require('./routes/projets');
 const elementsRoutes = require('./routes/elements'); // Ajoutez cette ligne
 
 router.use('/equipements', equipementsRoutes);
 router.use('/auth', authRoutes);
 router.use('/ouvertures', ouverturesRoutes);
-router.use('/projets', projetsRoutes);
 router.use('/elements', elementsRoutes); // Ajoutez cette ligne
 
 module.exports = router;
